50029d343d8f2634195e1b6a4e60f9af8470d2dd
Avinash Reddy <beavinash15@gmail.com>
2018-03-21 12:38:08 -0400

    Final submit

--- config/passport.js
+++ config/passport.js
@@ -13,7 +13,7 @@ module.exports = function(passport){
     }).then(user => {
       if(!user){
         return done(null, false, {message: 'No User Found'});
-      } 
+      }
 
       // Match password
       bcrypt.compare(password, user.password, (err, isMatch) => {
@@ -30,7 +30,7 @@ module.exports = function(passport){
   passport.serializeUser(function(user, done) {
     done(null, user.id);
   });
-  
+
   passport.deserializeUser(function(id, done) {
     User.findById(id, function(err, user) {
       done(err, user);
